7819d458dfcf1bf7939c113d38c7c528c2e26ec2
katagase-his <307902845+katagase-his@users.noreply.github.com>
2026-08-19 14:25:15 +0900

    Connect scanner to POC callback

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 /* Non-secret deployment configuration. Replace after the Apps Script deployment exists. */
 globalThis.HISTOIRE_POC_CONFIG = Object.freeze({
-  callbackUrl: '__APPS_SCRIPT_WEB_APP_URL__'
+  callbackUrl: 'https://script.google.com/macros/s/AKfycbwm_kox_VJy7BeAIHFlqGrUSZjpoU6uLdg6cVZFMRK9v1SvATDTN3tsSzIvFJr_JKLTWQ/exec'
 });
